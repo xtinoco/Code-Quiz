9c47423e293e23c55d2dd4fa2e0487801433f3a7
xochi <x.tinoco@yahoo.com>
2022-01-18 23:16:13 -0800

    saving changes

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
-const startBtn = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById('question-box')
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
-const answers
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-const shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
-startBtn.addEventListener('click', start)
+startButton.addEventListener('click', startGame)
 
-function start() {
-    console.log('started')
-    startBtn.classList.add('hide')
+function startGame() {
+    startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
-function nextQuestion() {
+function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+    const button = document.createElement('btn')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+        button.dataset.correct = answer.correct 
+    }
+    button.addEventListener('click', selectAnswer)
+    answersButtonsElement.appendChild(button)
+    })
 }
 
-function pickAnswer() {
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button)
+    })
+
 
 }
 
 const questions = [
     {
-        question: 'What does HTML stand for',
+        question: 'What is JavaScript?',
         answers: [
-            { text: 'hypertext markup language', correct: true},
-            { text: 'hightrack memory links', correct: false}
+            { text: 'A scripting language', correct: true},
+            { text: 'A popular coffee drink', correct: false}
         ]
 
     }
